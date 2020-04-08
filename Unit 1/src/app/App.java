17f835ffdab6bb0434a700722404d3f945b9984b
OwenEscher <818896@mystma.org>
2020-04-08 15:43:48 +0000

    Added top 5 movies

--- Unit 1/src/app/App.java
+++ Unit 1/src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1). Transformers movies");
+        System.out.println("2). Forest Gump");
+        System.out.println("3). Avengers Endgame");
+        System.out.println("4). Star wars The Last Jedi");
+        System.out.println("5). Pacific Rim");
     }
 }
