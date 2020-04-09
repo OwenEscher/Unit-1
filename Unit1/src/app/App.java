7f8f8b8303dfe97cac66d624428aecbea4762bac
OwenEscher <818896@mystma.org>
2020-04-09 16:17:22 +0000

    Added 8 data types

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,38 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte myFavNum = 21;
+        System.out.println("My favorite number is");
+        System.out.println(myFavNum);
+        boolean mintIsMyFavFlavorOfIcecream = true;
+        System.out.println("mint is my favorite flavor of ice cream");
+        System.out.println(mintIsMyFavFlavorOfIcecream);
+        char firstLetterOfMyLastName = 'E';
+        System.out.println("The fisrt letter of my last name is");
+        System.out.println(firstLetterOfMyLastName);
+        short howManyMilesIsItToTravelAroundTheWorld = 24901;
+        System.out.println("How many miles is it to travel around the world");
+        System.out.println(howManyMilesIsItToTravelAroundTheWorld);
+        int howManyYearsHaveDinosaursBeenExtinct = 65000000;
+        System.out.println("How many years have dinosaurs been extinct");
+        System.out.println(howManyYearsHaveDinosaursBeenExtinct);
+        long howManyCellsAreThereInTheAverageHumanBody = 37200000000000L;
+        System.out.println("On average how many cells are in the human body");
+        System.out.println(howManyCellsAreThereInTheAverageHumanBody);
+        float whatIsPie = 3.14F;
+        System.out.println("What is pie");
+        System.out.println(whatIsPie);
+        double betweenOneAndTenWhatNumberAmIThinkingOfRightNow = 2.1574937d;
+        System.out.println("Between 1 and 10 what number am I thinking of right now");
+        System.out.println(betweenOneAndTenWhatNumberAmIThinkingOfRightNow);
+
+
+
+
+
+
+
+
+    }
+}
