4906e240b149425bad04779e156f8f85ac8313ad
OwenEscher <818896@mystma.org>
2020-04-13 19:28:30 +0000

    mad libs complete

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,38 +1,25 @@
 package app;
 
+import java.security.acl.Owner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        byte myFavNum = 21;
-        System.out.println("My favorite number is");
-        System.out.println(myFavNum);
-        boolean mintIsMyFavFlavorOfIcecream = true;
-        System.out.println("mint is my favorite flavor of ice cream");
-        System.out.println(mintIsMyFavFlavorOfIcecream);
-        char firstLetterOfMyLastName = 'E';
-        System.out.println("The fisrt letter of my last name is");
-        System.out.println(firstLetterOfMyLastName);
-        short howManyMilesIsItToTravelAroundTheWorld = 24901;
-        System.out.println("How many miles is it to travel around the world");
-        System.out.println(howManyMilesIsItToTravelAroundTheWorld);
-        int howManyYearsHaveDinosaursBeenExtinct = 65000000;
-        System.out.println("How many years have dinosaurs been extinct");
-        System.out.println(howManyYearsHaveDinosaursBeenExtinct);
-        long howManyCellsAreThereInTheAverageHumanBody = 37200000000000L;
-        System.out.println("On average how many cells are in the human body");
-        System.out.println(howManyCellsAreThereInTheAverageHumanBody);
-        float whatIsPie = 3.14F;
-        System.out.println("What is pie");
-        System.out.println(whatIsPie);
-        double betweenOneAndTenWhatNumberAmIThinkingOfRightNow = 2.1574937d;
-        System.out.println("Between 1 and 10 what number am I thinking of right now");
-        System.out.println(betweenOneAndTenWhatNumberAmIThinkingOfRightNow);
-
-
-
-
-
-
-
-
+    String person1 = "Julian";
+    String person2 = "Westin";
+    String verb1 = "ran";
+    String verb2 = "grabbed";
+    String verb3 = "danced";
+    String food = "pizza";
+    String dayOfWeek = "Tuesday";
+    String place1 = "Supermarket";
+    String place2 = "Pizza Hut";
+    String adjective1 = "Purple";
+    String adjective2 = "smelly";
+    System.out.println(person1 + " " + verb2 + " the telephone to call " + place2 + "." );
+    System.out.println("He ordered " + person2 + "'s favorite kind of " + food + " which is pepperoni.");
+    System.out.println("When the delivery man arrived with the " + food + ", " + person1 + " and " + person2 + " " + verb1 + " to the door and " + verb3 + " around in excitment.");
+    System.out.println("Ready to eat, the two opened the " + food + " box, and to their suprise the " + food + " they ordered was " + adjective1 + " and " + adjective2 + "." );
+    System.out.println("Thankfully they live near a " + place1 + " which is always open on a " + dayOfWeek + ".");
+    
     }
 }
