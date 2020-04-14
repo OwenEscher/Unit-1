0a55d8e17db2e1bf2811590b5257415e0df6f093
OwenEscher <818896@mystma.org>
2020-04-14 18:29:56 +0000

    while loop assignment complete

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,25 +1,32 @@
 package app;
 
-import java.security.acl.Owner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-    String person1 = "Julian";
-    String person2 = "Westin";
-    String verb1 = "ran";
-    String verb2 = "grabbed";
-    String verb3 = "danced";
-    String food = "pizza";
-    String dayOfWeek = "Tuesday";
-    String place1 = "Supermarket";
-    String place2 = "Pizza Hut";
-    String adjective1 = "Purple";
-    String adjective2 = "smelly";
-    System.out.println(person1 + " " + verb2 + " the telephone to call " + place2 + "." );
-    System.out.println("He ordered " + person2 + "'s favorite kind of " + food + " which is pepperoni.");
-    System.out.println("When the delivery man arrived with the " + food + ", " + person1 + " and " + person2 + " " + verb1 + " to the door and " + verb3 + " around in excitment.");
-    System.out.println("Ready to eat, the two opened the " + food + " box, and to their suprise the " + food + " they ordered was " + adjective1 + " and " + adjective2 + "." );
-    System.out.println("Thankfully they live near a " + place1 + " which is always open on a " + dayOfWeek + ".");
-    
+        int count = 0;
+        String color = "red";
+        boolean theSkyIsBlue = false;
+
+
+       while (count < 10) {
+           System.out.println("The count is " + count);
+           count = count + 1;
+       }
+
+       System.out.println("count finished");
+
+       while (color == "red") {
+           System.out.println("The color is " + color);
+           color = "green";
+       }
+
+       System.out.println("color finished");
+
+       while (theSkyIsBlue == false) {
+           System.out.println("The sky is not blue. " + theSkyIsBlue);
+           theSkyIsBlue = true;
+       }
+
+       System.out.println("The sky is actually blue.");
+
     }
 }
