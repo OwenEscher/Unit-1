e6094227fba33dc066ce2de8bad39c89390f063f
OwenEscher <818896@mystma.org>
2020-04-15 14:51:48 +0000

    for loop assignment completed

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,31 +2,37 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int count = 0;
-        String color = "red";
-        boolean theSkyIsBlue = false;
+        
+       for (int k = 1000; k <= 256000; k = k +1){
+              System.out.println("The count is " + k);
+              System.out.println("Let's keep Counting");
+          }
+      
+       for (int i = 10; i <= 20; i = i +1){
+              System.out.println("The count is " + i);
+              System.out.println("Let's keep Counting");
+          }
+
+      for (int j = 10; j <= 100; j = j +1){
+              System.out.println("The count is " + j);
+              System.out.println("Let's keep Counting");
+          }
 
 
-       while (count < 10) {
-           System.out.println("The count is " + count);
-           count = count + 1;
-       }
+      for (int m = 11; m <= 12; m = m +1){
+              System.out.println("The count is " + m);
+              System.out.println("Let's keep Counting");
+          }
 
-       System.out.println("count finished");
+     for (int n = 10; n >= 0; n = n -1){
+              System.out.println("The count is " + n);
+              System.out.println("Let's keep Counting");
+          }
 
-       while (color == "red") {
-           System.out.println("The color is " + color);
-           color = "green";
-       }
 
-       System.out.println("color finished");
 
-       while (theSkyIsBlue == false) {
-           System.out.println("The sky is not blue. " + theSkyIsBlue);
-           theSkyIsBlue = true;
-       }
 
-       System.out.println("The sky is actually blue.");
+
 
     }
 }
