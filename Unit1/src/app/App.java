3026ed00558d854db517d740db72bf7b9f244bc7
OwenEscher <818896@mystma.org>
2020-04-23 15:38:31 +0000

    day 17 assignment done

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,36 +3,24 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
    
-        System.out.println("Start first loop");
-
-    for (int i = 0; i <= 19; i++) {
+    System.out.println("Start first loop");
+    for(int i = 20; i > 0; i = i - 1) {
         System.out.println(i);
-        if (i == 10) {
-            for (int j = 0; j < 5; j++) {
+        if (i == 10){
+            for (int j = 0; j < 7; j++){
                 System.out.println(i);
             }
         }
     }
-
-
-
-  
-    
-    
-        System.out.println("end first loop");
-    
-        System.out.println("Start second loop"); 
-      
-    for (int i = -1; i <= 19; i++) {
-        if (i % 3 == 0) {
-            
-        } else {
-            System.out.println(i);
+    System.out.println("End forst loop");
+    System.out.println("Start second loop");
+    for (int i = 0; i < 37; i = i + 4){
+        System.out.println(i);
+        if (i % 12 == 0){
+            System.out.println("Hey!");
         }
     }
-
     System.out.println("End second loop");
-
     }
 
 }
