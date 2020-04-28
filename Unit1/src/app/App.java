d9488ad5c59782bfa98a5f8a136181b539c44626
OwenEscher <818896@mystma.org>
2020-04-28 16:05:14 +0000

    Scanner asignment complete

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -1,26 +1,25 @@
 package app;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
    
-    System.out.println("Start first loop");
-    for (int i = 1; i <= 10; i = i + 1) {
-        System.out.println(i);
-        if (i == 5) {
-        System.out.println("five");
-        }
+Scanner input = new Scanner(System.in);
+
+System.out.println("Please enter your first name.");
+String firstName = input.nextLine();
+System.out.println("Please enter your last name.");
+String lastName = input.nextLine();
+System.out.println("Please enter your age.");
+String age = input.nextLine();
+System.out.println("Please enter your grade.");
+String grade = input.nextLine();
+System.out.println("Please enter your favorite color.");
+String color = input.nextLine();
+
+System.out.println("Hello " + firstName + " " + lastName + "! You are " + age + " years old, are in grade " + grade + ", and your favorite color is " + color + ".");
+
+
+
     }
-    System.out.println("End first loop");
-    System.out.println("Start second loop");
-    for (int i = 100; i <= 200; i = i + 10) {
-        if (i == 150) {
-            for (int j = 150; j <= 159; j = j + 1)
-            System.out.println(j);
-        }
-        else{
-            System.out.println(i);
-        }
-        }
-        System.out.println("End second loop");
-    }
+
 }
