7ff65c217e1cc4aa1879a1ba0e7601dbef55ed1e
OwenEscher <818896@mystma.org>
2020-05-05 18:07:35 +0000

    Quiz assignment complete

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -4,15 +4,35 @@ public class App {
     public static void main(String[] args) throws Exception {
    
 Scanner input = new Scanner(System.in);
-System.out.println("What is the count");
-int number = input.nextInt();
-for (int i = 1; i <= number; i = i + 1) {
-    System.out.println(i);
+
+String firstQ;
+String secondQ;
+String thirdQ;
+
+System.out.println("how many cats do I have?");
+firstQ = input.nextLine();
+if(firstQ.equals("three")){
+    System.out.println("Correct!");
+}else{
+    System.out.println("Wrong Answer :(");
 }
 
+System.out.println("What is my dads first name?");
+secondQ = input.nextLine();
+if(secondQ.equals("Jeff")){
+    System.out.println("Correct!");
+}else{
+    System.out.println("Worong Answer :(");
+}
 
+System.out.println("My first job was at what fast food restraunt?");
+thirdQ = input.nextLine();
+if(thirdQ.equals("Subway")){
+    System.out.println("Correct!");
+}else{
+    System.out.println("Wrong Answer :(");
+}
 
-
-    }
+}
 
 }
