11cb9d767a8e2448c984fd15462a7a54241b9ea9
OwenEscher <818896@mystma.org>
2020-05-07 16:28:40 +0000

    mad lib assignment complete

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,33 +5,81 @@ public class App {
    
 Scanner input = new Scanner(System.in);
 
-String firstQ;
-String secondQ;
-String thirdQ;
+System.out.println("Mad Libs Pizza Pizza");
 
-System.out.println("how many cats do I have?");
-firstQ = input.nextLine();
-if(firstQ.equals("three")){
-    System.out.println("Correct!");
-}else{
-    System.out.println("Wrong Answer :(");
-}
+System.out.println("Enter an adjective");
+String adjective = input.nextLine();
 
-System.out.println("What is my dads first name?");
-secondQ = input.nextLine();
-if(secondQ.equals("Jeff")){
-    System.out.println("Correct!");
-}else{
-    System.out.println("Worong Answer :(");
-}
+System.out.println("Enter a nationality");
+String nationality = input.nextLine();
 
-System.out.println("My first job was at what fast food restraunt?");
-thirdQ = input.nextLine();
-if(thirdQ.equals("Subway")){
-    System.out.println("Correct!");
-}else{
-    System.out.println("Wrong Answer :(");
-}
+System.out.println("Enter a persons name");
+String person = input.nextLine();
+
+System.out.println("Enter a noun");
+String noun = input.nextLine();
+
+System.out.println("Enter an adjective");
+String adjectiveTwo = input.nextLine();
+
+System.out.println("Enter a noun");
+String nounTwo = input.nextLine();
+
+System.out.println("enter an adjective");
+String adjectiveThree = input.nextLine();
+
+System.out.println("Enter another adjective");
+String adjectiveFour = input.nextLine();
+
+System.out.println("Enter a plural noun");
+String pluralNoun = input.nextLine();
+
+System.out.println("Enter a noun");
+String nounThree = input.nextLine();
+
+System.out.println("Enter a number (in words)");
+String number = input.nextLine();
+
+System.out.println("Enter a shape");
+String shape = input.nextLine();
+
+System.out.println("Enter a food");
+String food = input.nextLine();
+
+System.out.println("Enter another food");
+String foodTwo = input.nextLine();
+
+System.out.println("Enter a number (in words)");
+String numberTwo = input.nextLine();
+
+System.out.println("Pizza was invented by a " + adjective + " " + nationality + " chef named " + person + ".");
+System.out.println("To make pizza, you need to take a lump of " + noun + ", and make a thin, round " + adjectiveTwo + " " + nounTwo + ".");
+System.out.println("Then you cover it with " + adjectiveThree + " sauce, " + adjectiveFour + " cheese, and fresh chopped " + pluralNoun + ".");
+System.out.println("Next you have to bake it in a very hot " + nounThree + ".");
+System.out.println("When it is done, cut it into " + number + " " + shape + "s.");
+System.out.println("Some kids like " + food + " pizza the best, but my favorite is the " + foodTwo + " pizza.");
+System.out.println("If I could, I would eat pizza " + numberTwo + " times a day!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
